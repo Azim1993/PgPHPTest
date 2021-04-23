1b9f2b97d77c0ccc66c4c770f072fc3c41db5366
azim1993 <azimnurul1993@gmail.com>
2021-04-24 05:11:36 +0600

    User comment store by artisan comment test [ModelNotFound, succss] created

--- tests/Feature/UserCommentByArtisanCommandTest.php
+++ tests/Feature/UserCommentByArtisanCommandTest.php
@@ -2,21 +2,39 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class UserCommentByArtisanCommandTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_throws_not_found_exception_for_invalid_user_id()
     {
-        $response = $this->get('/');
+        $response = $this->artisan('user:comments', [
+            'USER_ID' => 5,
+            'COMMENT' => $this->faker->sentence(2)
+        ]);
 
-        $response->assertStatus(200);
+        $this->expectException(ModelNotFoundException::class, $response);
+    }
+
+    public function test_succesfully_store_comment_by_commands()
+    {
+        $user = User::factory()->create();
+        $response = Artisan::call('user:comments', [
+            'USER_ID' => $user->id,
+            'COMMENT' => $this->faker->sentence(2)
+        ]);
+
+        $this->assertEquals(1, $response);
     }
 }
