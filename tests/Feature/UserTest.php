692d007ddbe349e14c77c8702df59f91ff220401
azim1993 <azimnurul1993@gmail.com>
2021-04-24 05:35:25 +0600

    Test#Checked warning message if user id is missed from both query and params

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,21 +2,24 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_warning_mesage_if_userid_is_not_pass()
     {
-        $response = $this->get('/');
+        $response = $this->get('/users');
 
-        $response->assertStatus(200);
+        $response->assertSessionHas('warning');
     }
+
 }
