69db4713e0f2d582c44952917eb2d220a7e78cdb
azim1993 <azimnurul1993@gmail.com>
2021-04-24 06:33:04 +0600

    Test#Comment store unit test for comment is empty or not

--- tests/Unit/UserCommentUnitTest.php
+++ tests/Unit/UserCommentUnitTest.php
@@ -2,18 +2,34 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
 
 class UserCommentUnitTest extends TestCase
 {
+    use RefreshDatabase, WithFaker;
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_return_false_if_comment_is_null()
     {
-        
-        $this->assertTrue(true);
+        $user = User::factory()->make();
+        $commentStore = resolve(UserController::class)
+            ->storeCommentBy($user, '');
+        $this->assertFalse($commentStore);
+    }
+
+
+    public function test_return_true_if_comment_is_string()
+    {
+        $user = User::factory()->make();
+        $commentStore = resolve(UserController::class)
+            ->storeCommentBy($user, $this->faker->sentence());
+        $this->assertTrue($commentStore);
     }
 }
